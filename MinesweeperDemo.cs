fe30c0e17ee96040b90391da8479e35d186c67f3
Martin Spasov <martinspasof@gmail.com>
2013-05-11 20:09:44 +0300

    Update and rename MinesweeperTest.cs to MinesweeperDemo.cs
    change name file and add namespace

---
+++ MinesweeperDemo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+namespace minesweeper
+{
+    class MinesweeperDemo
+    {
+        public static void Main(string[] args)
+        {
+            Mines game = new Mines();
+            game.PlayMines();
+        }
+    }
+}
