83ae66bf2fb3819ae7083c573c7d9041b6a44ec0
Assi <Assi.Ivomir@gmail.com>
2013-05-17 00:05:48 +0300

    AddNewUnitTest for method AddPlayer()

---
+++ MineSweeper/MineSweeper/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Minesweeper.Properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Minesweeper.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3753f644-af5f-4c02-aa5f-ce4017f55c78")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
+[assembly: InternalsVisibleTo("UnitTestProject1")]
---
+++ MineSweeper/MineSweeper/TEST.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Minesweeper
+{
+    class TEST
+    {
+        public static void Main(string[] args)
+        {
+            
+
+            var output = new StringBuilder();
+            var textWriter = new StringWriter(output);
+            Console.SetOut(textWriter);
+
+            Console.WriteLine("TEST");
+
+            Console.WriteLine();
+        }
+
+        
+    }
+}
